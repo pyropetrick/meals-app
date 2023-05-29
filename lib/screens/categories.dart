e73b3ec93d33dbf3a3e84a5142003919c8db2994
Pavel <pavel.chernenko97@gmail.com>
2023-05-29 10:41:00 +0300

    feat: add all screens, tabs and drawer

---
+++ lib/screens/categories.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '/models/category.dart';
+import 'meals.dart';
+import '/widgets/category_grid_item.dart';
+import '../models/meal.dart';
+import '/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({
+    super.key,
+    required this.onToogleFavorite,
+    required this.availableMeals,
+  });
+
+  final List<Meal> availableMeals;
+
+  final void Function(Meal meal) onToogleFavorite;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+          onToogleFavorite: onToogleFavorite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
