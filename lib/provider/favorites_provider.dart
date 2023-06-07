3e724e6c99eb1b47444897027d50b8b760e21d83
Pavel <pavel.chernenko97@gmail.com>
2023-06-07 13:44:29 +0300

    feat: create a riverpod providers for app

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  bool toogleMealFavoriteStatus(Meal meal) {
+    final isFavoriteMeal = state.contains(meal);
+
+    if (isFavoriteMeal) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+  (ref) => FavoriteMealsNotifier(),
+);
